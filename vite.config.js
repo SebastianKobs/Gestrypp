ee69bec7a1085292c5648dedb407b00bb2483d81
Sebastian Kobs <kobs@ewerk.de>
2025-07-04 12:46:07 +0200

    3d test export vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     base: '/Gestrypp/',
+    publicDir: 'assets',
     build: {
         rollupOptions: {
             input: {
