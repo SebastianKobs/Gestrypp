2e319564c82efd0178500e3537c18c09d879cefc
Sebastian Kobs <kobs@ewerk.de>
2025-07-04 12:32:40 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,17 @@
-export default {
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
     base: '/Gestrypp/',
-}
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                nested: resolve(__dirname, '3dtest.html'),
+            },
+        },
+    },
+});
